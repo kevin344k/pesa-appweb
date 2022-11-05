d8ed84d31a510ebc925e4e5d5e4d0196e6c13818
kevin344k <kevin344k@gmail.com>
2022-11-05 01:40:52 -0500

    ya hice la pantalla para la orden de producción

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,19 @@ const express=require('express')
 const router=express.Router()
 
 
+// cuando recien se carga la pagina va ha soliciatr una ruta, que será la ruta raíz y el código de abajo permite responder con la renderizacion de la página principal
+
 router.get('/',(req,res)=>{
    res.render("index")
+})
+
+// Cuando s ed eclic en submit se en el formulario de planeación el código de abajo va a renderizar en respuesta a esa solicitud, la página orden de propducción 
+router.get('/ordenProd',(req,res)=>{
+   res.render("orden_prod.hbs")
 })
 
 
 
 
+// este código permite exportar el módulo router al archivo autentication.js
 module.exports=router
