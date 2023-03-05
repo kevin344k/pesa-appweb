538f6df63dbcf4d83ba086fab49f935a14ee3f79
kevin344k <kevin344k@gmail.com>
2023-03-05 14:21:45 -0500

    prueba 3 de sockets

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require("express"); //se importa express para iniciar el server-
 const http = require("http");//----------
 const exphbs = require("express-handlebars"); //permite renderizar hbs
 const morgan = require("morgan"); //crea logs de las peticiones del cliente al servidor
-const { Server } = require("socket.io");
+const {Server} = require('socket.io');
 const path = require("path"); // permite manejar rutas internas de archivos-------------
+const PORT=process.env.PORT || 5000
 //BASE DE DATOS
 
 //INICIALIZACIONES
@@ -11,7 +12,7 @@ const app = express(); //se inicializa express------------------
 
 //pasando el server a http
 
-const httpServer = http.createServer(app);
+const server = http.createServer(app);
 
 //static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,11 +20,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //SETTINGS
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", PORT);
 
 //websockets
 
-const io = new Server(httpServer);
+const io = new Server(server);
 require("./sockets")(io);
 
 ///// se configuar las carpetas con los "hbs" para ser cargado cada ves que se haga una peticion del cliente/////
@@ -68,5 +69,5 @@ app.use("/app", require("./routes/app"));
 
 //STARTING THE SERVER con express
 
-httpServer.listen(app.get("port"));
-console.log("server on port", app.get("port"));
+server.listen(app.get("port"));
+console.log(`server on port, ${PORT}`);
